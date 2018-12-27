0e6ca9b02a86a5f447b5a387fc94dca0009f1fe8
TestUser <kartik@example.com>
2018-12-27 21:54:40 +0530

    my first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = Add(10, 20);
+        }
+
+
+        public static int Add(int v1, int v2)
+        {
+            return v1+v2;
+        }
+        public  int Add1(int v1, int v2)
+        {
+            return v1 + v2;
+        }
+    }
+}
